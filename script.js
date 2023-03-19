280c391c02ad4d95ec022a2808dd698477ad5c4b
lautarosaya <lautarosaya0012@gmail.com>
2023-03-19 08:36:46 -0300

    js-front clase 44 terminada

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 console.log("JS desde Front");
 const nombre = "Juana";
 console.log(nombre + " sabe JS");
+
+let h1 = document.querySelector("h1");
+if (h1 != null) {
+  console.log(h1);
+}
+
+let h1id = document.querySelector("#title");
+if (h1id != null) {
+  console.log(h1);
+}
+
+let lorem = document.querySelector(".lorem-1");
+if (lorem != null) {
+  console.log(lorem);
+}
+
+let todoslosdiv = document.querySelectorAll("div");
+if (todoslosdiv != null) {
+  console.log(todoslosdiv);
+  todoslosdiv.forEach((div) => {
+    if (div == document.querySelector("#segundo")) {
+      console.log("Este es el div buscado " + div.id);
+    }
+  });
+}
+
+let navAnchors = document.querySelectorAll("#main-navbar");
+console.log(navAnchors);
+
+const listItems = document.querySelectorAll("ul#cont01 > li");
+console.log(listItems);
+
+for (let i = 0; i < listItems.length; i++) {
+  if (listItems[i].classList.contains("alert-danger")) {
+    listItems[i].classList.remove("alert-danger");
+    listItems[i].classList.add("alert-success");
+  }
+}
